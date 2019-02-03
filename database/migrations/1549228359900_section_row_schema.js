4ca717197f2b8b0b7552292cb1224d424dfcbce8
Kalle Bornemark <kalle.bornemark@gmail.com>
2019-02-03 22:23:52 +0100

    core: make columns in SectionRow nullable

---
+++ database/migrations/1549228359900_section_row_schema.js
@@ -0,0 +1,20 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class SectionRowSchema extends Schema {
+  up () {
+    this.table('section_rows', (table) => {
+      table.string('category', 80).alter()
+    })
+  }
+
+  down () {
+    this.table('section_rows', (table) => {
+      // reverse alternations
+    })
+  }
+}
+
+module.exports = SectionRowSchema
